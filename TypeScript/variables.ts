24f7b51fc2df323b6f573b0987f6373292a1fd47
katemalets <katyamask@gmail.com>
2020-06-16 10:17:07 +0300

    defining variables

---
+++ TypeScript/variables.ts
@@ -0,0 +1,8 @@
+let found : boolean = true;
+let rating : number = 10;
+let firstName : string = "Kate";
+let lastName : string = 'Malets';
+
+console.log(found);
+console.log("Rating: " + rating);
+console.log("Initials: " + firstName + lastName);
