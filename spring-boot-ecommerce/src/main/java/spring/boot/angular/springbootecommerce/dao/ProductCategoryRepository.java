23a0da6f7cde0802ff10d5cb5cde4b7ef3ce1d57
katemalets <katyamask@gmail.com>
2020-06-19 14:42:00 +0300

    integrating angular and spring boot

--- spring-boot-ecommerce/src/main/java/spring/boot/angular/springbootecommerce/dao/ProductCategoryRepository.java
+++ spring-boot-ecommerce/src/main/java/spring/boot/angular/springbootecommerce/dao/ProductCategoryRepository.java
@@ -2,8 +2,10 @@ package spring.boot.angular.springbootecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import spring.boot.angular.springbootecommerce.entity.ProductCategory;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-categories")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory,Long> {
 }
--- spring-boot-ecommerce/src/main/java/spring/boot/angular/springbootecommerce/dao/ProductRepository.java
+++ spring-boot-ecommerce/src/main/java/spring/boot/angular/springbootecommerce/dao/ProductRepository.java
@@ -1,7 +1,9 @@
 package spring.boot.angular.springbootecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import spring.boot.angular.springbootecommerce.entity.Product;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
