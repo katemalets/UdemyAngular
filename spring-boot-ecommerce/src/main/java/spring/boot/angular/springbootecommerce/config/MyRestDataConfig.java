077891c8818445ff6b8523b37cdb0b2c22298062
katemalets <katyamask@gmail.com>
2020-06-23 09:47:08 +0300

    modify spring boot app

--- spring-boot-ecommerce/src/main/java/spring/boot/angular/springbootecommerce/config/MyRestDataConfig.java
+++ spring-boot-ecommerce/src/main/java/spring/boot/angular/springbootecommerce/config/MyRestDataConfig.java
@@ -1,5 +1,6 @@
 package spring.boot.angular.springbootecommerce.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -7,8 +8,21 @@ import org.springframework.http.HttpMethod;
 import spring.boot.angular.springbootecommerce.entity.Product;
 import spring.boot.angular.springbootecommerce.entity.ProductCategory;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MyRestDataConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public MyRestDataConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
@@ -24,5 +38,16 @@ public class MyRestDataConfig implements RepositoryRestConfigurer {
                 .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedActions)))
                 .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedActions)));
 
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+        List<Class> entityClasses = new ArrayList<>();
+        for(EntityType entity: entities){
+            entityClasses.add(entity.getJavaType());
+        }
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
     }
 }
