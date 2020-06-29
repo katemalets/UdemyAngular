e7f611815c42a337505425ba544d16856458cc86
katemalets <katyamask@gmail.com>
2020-06-29 10:23:35 +0300

    retrieve data from cart service

--- ecommerce-frontend/src/app/components/cart-details/cart-details.component.ts
+++ ecommerce-frontend/src/app/components/cart-details/cart-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {CartItem} from "../../common/cart-item";
+import {CartService} from "../../services/cart.service";
 
 @Component({
   selector: 'app-cart-details',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartDetailsComponent implements OnInit {
 
-  constructor() { }
+  cartItems: CartItem[] = [];
+  totalQuantity: number = 0;
+  totalPrice: number = 0;
+
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
+    this.listCartDetails();
   }
 
+  listCartDetails() {
+
+    this.cartItems = this.cartService.cartItems;
+
+    this.cartService.totalPrice.subscribe(
+      data => this.totalPrice = data
+    );
+
+    this.cartService.totalQuantity.subscribe(
+      data => this.totalQuantity = data
+    );
+
+    this.cartService.computeCartTotals();
+  }
 }
