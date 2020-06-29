5f8312105f3769c28ad5ef072eef1df68cc34a2b
katemalets <katyamask@gmail.com>
2020-06-29 11:42:28 +0300

    add event handlers

--- ecommerce-frontend/src/app/components/cart-details/cart-details.component.ts
+++ ecommerce-frontend/src/app/components/cart-details/cart-details.component.ts
@@ -33,4 +33,8 @@ export class CartDetailsComponent implements OnInit {
 
     this.cartService.computeCartTotals();
   }
+
+  incrementQuantity(cartItem : CartItem){
+    this.cartService.addToCart(cartItem);
+  }
 }
